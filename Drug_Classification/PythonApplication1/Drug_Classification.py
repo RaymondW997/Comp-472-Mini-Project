43866943018244278f9c0cdeac95101a5ae9a67d
RaymondW997 <raymondwu997@gmail.com>
2021-10-15 19:35:00 -0400

    added all classifier info and implemented data plotting

--- Drug_Classification/PythonApplication1/Drug_Classification.py
+++ Drug_Classification/PythonApplication1/Drug_Classification.py
@@ -14,7 +14,8 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
-
+from sklearn.utils._testing import ignore_warnings
+from sklearn.exceptions import ConvergenceWarning
 
 pd.set_option('display.max_columns', None)
 
@@ -22,15 +23,38 @@ pd.set_option('display.max_columns', None)
 def load_data():
     return pd.read_csv("Datasets/drug200.csv")
 
+#Loading the dataFrame into the variable df
+df = load_data()
 
-#plotting the classes
-#print(df)
-#data.plot(kind = 'scatter', x = 'Sex', y = 'Drug', color = 'red')
-#plt.show() 
+#storing the drug column into the variable drugCol
+drugCol = df.iloc[:,-1:].values
+drugList = []
+
+#Appending unique drug names into a new list and sorting it 
+#in alphabetical order
+for drug in drugCol:
+    if drug[0] not in drugList:
+        drugList.append(drug[0])
+
+sortedDrugList = sorted(drugList)
+
+drugFrequency = [0] * len(sortedDrugList)
+
+#For loop that will count the number of every drug type
+for drug in drugCol:
+    index = sortedDrugList.index(drug[0])
+    drugFrequency[index] = drugFrequency[index] + 1
+
+#Plotting the frequency of each drug type
+plt.figure(figsize=(9, 5))
+plt.xlabel('Drugs')
+plt.ylabel('Instances')
+plt.bar(sortedDrugList, drugFrequency)
+plt.suptitle('Frequency of Drugs')
+plt.savefig("drug-distribution.pdf")
 
 
 #Using pandas.get_dummies to convert the ordinal and nomnal features in numerical format
-df = load_data()
 df = pd.get_dummies(df, columns=['Sex', 'BP', 'Cholesterol'])
 #print(df.head())
 
@@ -69,6 +93,20 @@ predictionBase_DT = classifierBase_DT.predict(x_test)
 print("Decision Prediction Results: ")
 print(predictionBase_DT)
 
+#c) Using Top-DT classifier to predict the split dataset
+#   and will print out the predicted outcome of the Top-DT
+#  classifier
+param_TopDT = {'criterion':['entropy'], 
+               'max_depth': [5, 20], 
+               'min_samples_split':[3, 6, 9]}
+
+classifierTop_DT = GridSearchCV(DecisionTreeClassifier(), param_TopDT)
+classifierTop_DT.fit(x_train, y_train)
+
+predictionTop_DT = classifierTop_DT.predict(x_test)
+print("Top-DT Results: ")
+print(predictionTop_DT)
+
 
 #d) Using Perceptron classifier to predict the split dataset
 #   and will print out the predicted outcome of the perceptron
@@ -87,6 +125,22 @@ print(predictionPercep)
 classifierBase_MLP = MLPClassifier(activation='logistic', solver='sgd')
 classifierBase_MLP.fit(x_train, y_train)
 
-preditionBase_MLP = classifierBase_MLP.predict(x_test)
+predictionBase_MLP = classifierBase_MLP.predict(x_test)
 print("MLP Prediction Results: ")
-print(preditionBase_MLP)
+print(predictionBase_MLP)
+
+
+#f) Using Top-MLP classifier to predict the split dataset
+#   and will print out the predicted outcome of the Top-MLP
+#   classifier
+param_TopMLP = {'activation': ['tanh', 'relu', 'identity'],
+               'hidden_layer_sizes': [(30, 50, 150), (8, 24, 116)],
+               'solver': ['adam', 'sgd']        
+               }
+
+classifierTop_MLP = GridSearchCV(estimator = MLPClassifier(max_iter=200), param_grid = param_TopMLP, n_jobs=-1)
+classifierTop_MLP.fit(x_train, y_train)
+
+predictionTop_MLP = classifierTop_MLP.predict(x_test)
+print("Top-MLP Prediction Results: ")
+print(predictionTop_MLP)
